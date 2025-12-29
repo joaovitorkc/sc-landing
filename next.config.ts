d6e0aa543d686ad9559d3f33b41c98f419a0681f
João Vitor <jv0488598@gmail.com>
2025-12-29 10:10:28 -0300

    remove rewrites

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,7 @@
 import type { NextConfig } from 'next';
 
-const { NEXT_PUBLIC_API_URL } = process.env;
-
 const nextConfig: NextConfig = {
-  distDir: process.env.BUILD_DIR || '.next',
-  rewrites: async () => {
-    return [
-    {
-      source: '/arquivos/:resource*',
-      destination: `${NEXT_PUBLIC_API_URL}/attachments/:resource*`,
-    },
-    {
-      source: '/api/:resource*',
-      destination: `${NEXT_PUBLIC_API_URL}/:resource*`,
-    },
-  ]}
+  distDir: process.env.BUILD_DIR || '.next'
 };
 
 export default nextConfig;
